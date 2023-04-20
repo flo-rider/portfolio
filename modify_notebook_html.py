4cc0f039dda2cb423a78a26a531a7ef6bcb7e7a8
flo-rider <floriah@gmail.com>
2023-04-21 00:06:44 +0100

    updated notebooks

--- modify_notebook_html.py
+++ modify_notebook_html.py
@@ -2,7 +2,7 @@ import bs4
 import sys
 
 '''run this in terminal first: 
-jupyter nbconvert --to html --TagRemovePreprocessor.remove_cell_tags 'hide_code' pokemon/clustering.ipynb'''
+jupyter nbconvert --to html --TagRemovePreprocessor.remove_cell_tags 'hide_code' pokemon/ignore-clustering.ipynb'''
 
 html_path = sys.argv[1]
 new_title = sys.argv[2]
